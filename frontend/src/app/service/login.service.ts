e7bf03a0a8d6ea67b6e1da2eb16d3d6b49f1001e
Rishabh <rishabhgupta7199@gmail.com>
2022-12-11 19:07:20 +0530

    added get method in loginservice

--- frontend/src/app/service/login.service.ts
+++ frontend/src/app/service/login.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../model/user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
+  
+  get(user: User) {
+    this.http.get('http://localhost:8085/login')
+  }
 }
