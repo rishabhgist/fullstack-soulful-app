fce34caf1f4b2fb1452d75ada99a419a2830b868
Rishabh <rishabhgupta7199@gmail.com>
2022-12-11 19:29:14 +0530

    created formgroup and added validations

--- frontend/src/app/dashboard/signup/signup.component.ts
+++ frontend/src/app/dashboard/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ValidationErrors, ValidatorFn, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  customer = this.fb.group({
+    customerName: ['', Validators.required],
+    customerPhone: ['', [
+          Validators.required,
+          Validators.pattern('(9|8|7)[0-9]{9}'),
+        ]],
+    email: ['', Validators.compose([
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ])],
+    password: ['', Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d\\W]{8,63}$'
+          ),
+        ])],
+    cnfPassword:['', Validators.required]
+  },  { validator: verifypwd('password', 'cnfPassword') })
+
+}
+// Verfiy confirm Password
+export function verifypwd(pass: string, cnfPass: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get(pass)?.value;
+    const cnfPassword = control.get(cnfPass)?.value;
+    if (password != cnfPassword) {
+      const err = { 'noMatch': true };
+      control.get(cnfPass)?.setErrors(err);
+      return err;
+    } 
+    return null;
+  }
 }
