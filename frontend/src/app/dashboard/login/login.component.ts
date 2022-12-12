e5eb2faf73a4bacf1af774664205c0c3ba6f69e1
Rishabh <rishabhgupta7199@gmail.com>
2022-12-12 17:37:23 +0530

    added form validations and error messages

--- frontend/src/app/dashboard/login/login.component.ts
+++ frontend/src/app/dashboard/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-
+  
+  user = this.fb.group({
+    email: ['', Validators.compose([
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ])],
+    password: ['', Validators.required]
+  })
+  
 }
