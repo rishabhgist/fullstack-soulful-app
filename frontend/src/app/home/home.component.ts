20211790f222b3768b15bc92cdc1d5ac2ba4eb84
Rishabh <rishabhgupta7199@gmail.com>
2022-12-12 20:35:23 +0530

    added brand logo

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -10,6 +10,61 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.profile = this.profiles.at(this.key);
   }
 
+  profiles: Profile[] = [
+    {
+      name: "Sneha Shinde",
+      city: "Pune, Maharashtra",
+      age: "23",
+      profile:"sneha" 
+    },
+    {
+      name: "Dimple Goyal",
+      city: "Mumbai, Maharashtra",
+      age: "24",
+      profile: "dimple"
+    },
+    {
+      name: "Akansha Yadav",
+      city: "Delhi",
+      age: "20",
+      profile: "sneha"
+    },
+    {
+      name: "Deepika Bhardwaj",
+      city: "Dehradun, Uttrakhand",
+      age: "25",
+      profile: "deepika"
+    },
+    {
+      name: "Meera Bai",
+      city: "Bengluru, Karanataka",
+      age: "23",
+      profile: "meera"
+    }
+  ]
+  profile: Profile | undefined = {};
+  key: number = 0;
+
+  next() {
+    if (this.profiles.length-1 > this.key) {
+      if (this.profiles != undefined) {
+        this.key ++;
+        this.profile = this.profiles.at(this.key);
+        console.log(this.profiles.length + '' + this.key);
+
+      }
+    }else {
+        this.key = 0;
+    }
+  }
+}
+
+export type Profile = {
+  name?: String,
+  city?: String, 
+  age?: String,
+  profile?: String
 }
