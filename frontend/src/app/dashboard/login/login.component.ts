aa344f5c0c44c886019acfb7798dfb5348d8b934
Rishabh <rishabhgupta7199@gmail.com>
2022-12-13 12:44:34 +0530

    changed login style

--- frontend/src/app/dashboard/login/login.component.ts
+++ frontend/src/app/dashboard/login/login.component.ts
@@ -11,14 +11,16 @@ export class LoginComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
   }
-  
+
   user = this.fb.group({
     email: ['', Validators.compose([
           Validators.required,
           Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
         ])],
     password: ['', Validators.required]
+    
   })
   
 }
