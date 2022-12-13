f2ff4389cca78e1d2ae4a91d3d3a345c9ff77e2e
Rishabh <rishabhgupta7199@gmail.com>
2022-12-13 18:55:01 +0530

    completed auth gaurd ts

--- frontend/src/app/gaurd/auth.guard.ts
+++ frontend/src/app/gaurd/auth.guard.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, retry } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+import { LoginService } from '../service/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router:Router, private loginService:LoginService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.authService.getToken() !== null) {
+      const user = route.data["username"];
+      if (user !== null) {
+        const match = this.loginService.userMatch(user);
+        if (match) {
+          return true;
+        } else {
+          this.router.navigate(['/forbidden']);
+          return false;
+        }
+      }
+    }
+    this.router.navigate(['/login']);
+    return false;
   }
   
 }
